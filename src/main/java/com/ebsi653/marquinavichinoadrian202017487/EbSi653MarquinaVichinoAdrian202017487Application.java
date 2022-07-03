e8103b7ff2e161289ca0a3a1d4af99bb9d72f96f
Cobollatin <Cobollatin@hotmail.com>
2022-07-03 15:53:15 -0500

    setup

---
+++ src/main/java/com/ebsi653/marquinavichinoadrian202017487/EbSi653MarquinaVichinoAdrian202017487Application.java
@@ -0,0 +1,13 @@
+package com.ebsi653.marquinavichinoadrian202017487;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbSi653MarquinaVichinoAdrian202017487Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbSi653MarquinaVichinoAdrian202017487Application.class, args);
+	}
+
+}
